9a788a6de76721101ed2ddd5084830664f1aa93d
s3545999 <s3545999@student.rmit.edu.au>
2021-05-05 21:29:19 +1000

    Starting Forgot Password

---
+++ src/main/controller/ForgotPasswordController.java
@@ -0,0 +1,68 @@
+package main.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import main.model.ForgotPasswordModel;
+import main.model.LoginModel;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ForgotPasswordController implements Initializable {
+    public ForgotPasswordModel forgotPasswordModel = new ForgotPasswordModel();
+    @FXML
+    private Label isConnected;
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private TextField txtPassword;
+
+
+    // Check database connection
+    @Override
+    public void initialize(URL location, ResourceBundle resources){
+        if (loginModel.isDbConnected()){
+            // isConnected.setText("Connected");
+        }else{
+            isConnected.setText("Not Connected");
+        }
+
+    }
+    /* login Action method
+       check if user input is the same as database.
+     */
+    public void Login(ActionEvent event){
+
+        try {
+            if (loginModel.isLogin(txtUsername.getText(),txtPassword.getText())){
+
+                isConnected.setText("Logged in successfully");
+            }else{
+                isConnected.setText("username and password is incorrect");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createAccountButtonPushed(ActionEvent event) throws IOException {
+
+        Parent createAccParent = FXMLLoader.load(getClass().getResource("../ui/createAccount.fxml"));
+        Stage newStage = new Stage();
+        newStage.setScene(new Scene(createAccParent, 600, 400));
+        newStage.show();
+
+        final Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.close();
+    }
+}
---
+++ src/main/model/ForgotPasswordModel.java
@@ -0,0 +1,61 @@
+package main.model;
+
+import main.SQLConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class ForgotPasswordModel {
+
+    Connection connection;
+
+    public ForgotPasswordModel(){
+
+
+        connection = SQLConnection.connect();
+        if (connection == null)
+            System.exit(1);
+
+    }
+
+    public Boolean isDbConnected(){
+        try {
+            return !connection.isClosed();
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+
+    public Boolean isLogin(String user, String pass) throws SQLException {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet=null;
+        String query = "select * from Employee where username = ? and password= ?";
+        try {
+
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, user);
+            preparedStatement.setString(2, pass);
+
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            return false;
+        }finally {
+           preparedStatement.close();
+           resultSet.close();
+        }
+
+    }
+
+}
